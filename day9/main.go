74c1bb2d110016f268b68c16dc79d5904797615b
Sarah Padovani <sarah.padovani@redspace.com>
2021-12-10 15:58:12 -0400

    [day 9] part 2

--- day9/main.go
+++ day9/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -31,51 +31,68 @@ func main() {
 		caves = append(caves, cave)
 	}
 
-	var lowPoints []int64
+	var basins []int
 
-	for caveIndex, cave := range caves {
-		// if the caveIndex is 0, we don't have upper locations
-		var hasTop = caveIndex > 0
-		// if this is the last cave, we don't have lower locations
-		var hasBottom = caveIndex < len(caves)-1
+	for caveIndex := 0; caveIndex < len(caves); caveIndex++ {
+		for locationIndex := 0; locationIndex < len(caves[caveIndex]); locationIndex++ {
+			basin := GetBasinSize(&caves, caveIndex, locationIndex)
 
-		for locationIndex, location := range cave {
-			// use 10 as our empty state, since all of our locations are 9 and lower
-			lowest := float64(10)
-
-			// if the locationIndex is 0, we don't have a left neighbor
-			var hasLeft = locationIndex > 0
-			// if it's the last location, we don't have a right neighbor
-			var hasRight = locationIndex < len(cave)-1
-
-			if hasTop {
-				lowest = math.Min(lowest, float64(caves[caveIndex-1][locationIndex]))
-			}
-
-			if hasBottom {
-				lowest = math.Min(lowest, float64(caves[caveIndex+1][locationIndex]))
-			}
-
-			if hasLeft {
-				lowest = math.Min(lowest, float64(cave[locationIndex-1]))
-			}
-
-			if hasRight {
-				lowest = math.Min(lowest, float64(cave[locationIndex+1]))
-			}
-
-			// we only want to consider this a low point if it's LOWER, not equal to, its neighbours
-			if location < int64(lowest) {
-				lowPoints = append(lowPoints, location)
+			if basin > 0 {
+				basins = append(basins, basin)
 			}
 		}
 	}
 
-	var total int64
+	sort.Sort(sort.Reverse(sort.IntSlice(basins)))
 
-	for _, value := range lowPoints {
-		total += value + 1
+	var total int = basins[0]
+
+	for _, value := range basins[1:3] {
+		total *= value
 	}
 
 	fmt.Println(total)
+}
+
+// caves needs to be a pointer, so that we can mutate the collection
+func GetBasinSize(caves *[][]int64, caveIndex int, locationIndex int) int {
+	var basinSize int
+	cave := (*caves)[caveIndex]
+	location := cave[locationIndex]
+
+	// if this number isn't 9, recursively find all of its non-9 neighbours
+	if location >= 9 {
+		return 0
+	}
+
+	// as we identify pieces that belong to a basin, change their value to 10 so that we don't count them twice
+	basinSize++
+	(*caves)[caveIndex][locationIndex] = 10
+
+	// if the caveIndex is 0, we don't have upper locations
+	var hasTop = caveIndex > 0
+	// if this is the last cave, we don't have lower locations
+	var hasBottom = caveIndex < len(*caves)-1
+	// if the locationIndex is 0, we don't have a left neighbor
+	var hasLeft = locationIndex > 0
+	// if it's the last location, we don't have a right neighbor
+	var hasRight = locationIndex < len(cave)-1
+
+	if hasTop && (*caves)[caveIndex-1][locationIndex] < 9 {
+		basinSize += GetBasinSize(caves, caveIndex-1, locationIndex)
+	}
+
+	if hasBottom && (*caves)[caveIndex+1][locationIndex] < 9 {
+		basinSize += GetBasinSize(caves, caveIndex+1, locationIndex)
+	}
+
+	if hasLeft && cave[locationIndex-1] < 9 {
+		basinSize += GetBasinSize(caves, caveIndex, locationIndex-1)
+	}
+
+	if hasRight && cave[locationIndex+1] < 9 {
+		basinSize += GetBasinSize(caves, caveIndex, locationIndex+1)
+	}
+
+	return basinSize
 }
